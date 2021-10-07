96b2476abc2add929b31ae5a2c084b6dff6944c3
brokenSensor <jonimen95@yandex.ru>
2021-10-07 10:57:12 +0800

    Fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
   ssr: false,
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
+  router: {
+    base: "/vue-todo/"
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
